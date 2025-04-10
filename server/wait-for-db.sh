1094ae27089e5e549106112687f79ccaf061c7d4
caiquemiranda <caiquemirandas@gmail.com>
2025-04-10 20:58:20 -0300

    Adiciona o cliente MySQL ao Dockerfile e garante permissão de execução para o script wait-for-db.sh, que aguarda a conexão com o banco de dados antes de iniciar o servidor.

---
+++ server/wait-for-db.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# wait-for-db.sh
+
+set -e
+
+host="$1"
+shift
+cmd="$@"
+
+until mysql -h "$host" -u user -ppassword -e 'SELECT 1'; do
+  >&2 echo "MySQL ainda não está disponível - aguardando..."
+  sleep 5
+done
+
+>&2 echo "MySQL está pronto - executando comando"
+exec $cmd 
